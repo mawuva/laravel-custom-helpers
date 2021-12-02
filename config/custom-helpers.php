eea92a6ab58b5ee98b74ae34a4eb6d9a47c1beeb
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-02 15:25:20 +0100

    Feat: Add slug separator config

--- config/custom-helpers.php
+++ config/custom-helpers.php
@@ -1,8 +1,16 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Slug Separator
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the slug separator. This is very important in matter
+    | of magic method __call() and also a `Slugable` trait. The default value
+    | is a dot.
+    |
+    */
 
+    'separator' => env('ACCONTROL_DEFAULT_SEPARATOR', '-'),
 ];
